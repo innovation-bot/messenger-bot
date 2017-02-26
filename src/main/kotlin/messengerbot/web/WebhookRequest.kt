aa586cba63a672a98ab73654d4c7aa3725d80f0b
yusuke iwanaga <yusukeiwanaga@yusukenoMacBook-Air.local>
2017-02-26 20:38:23 +0900

    test

--- src/main/kotlin/messengerbot/web/WebhookRequest.kt
+++ src/main/kotlin/messengerbot/web/WebhookRequest.kt
@@ -3,7 +3,6 @@ package messengerbot.web
 import com.fasterxml.jackson.annotation.JsonCreator
 import com.fasterxml.jackson.annotation.JsonProperty
 
-@JsonCreator
 data class WebhookRequest(val entry: List<Entry>)
 
 data class Entry(val id: String, val time: Long, val messaging: Messaging)
